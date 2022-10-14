ee9381c333d86c087604289439111a4a833e1e27
sumu-thara <sumudu.2018348@iit.ac.lk>
2022-10-15 03:22:34 +0530

    I created my Blog app

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="plugin/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@600&display=swap" rel="stylesheet">
+    <script src="plugin/jquery.min.js"></script>
+    <script src="plugin/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    include_once('ioc/navbar.php');
+          
+  ?> 
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="jumbotron mt-5">
+                <h2 id="jumbo-header">Welcome to Aesthetics</h2>
+                <div class="imagegal"><img src="images/gallery.png" height="100"></div>
+
+            </div>
+        </div>
+    </div>
+
+</div> 
+
+</body>
+</html>
